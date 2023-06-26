f3bedc034ae39cd7dc4b8de63ce877f9cf701c67
Jared <jaredhessworks@gmail.com>
2023-06-26 16:53:15 -0400

    fixed grid proportions while growing & shrinking

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const gridContainer = document.querySelector('.gridContainer');
 
 let gridSize = 16;
 
-let squareSize = 100;
+let squareSize = 960 / gridSize;
+
+
 for(let i = 0; i < gridSize; i++){
     const i = document.createElement('div');
     i.classList.add('parentSquares');
@@ -14,7 +16,10 @@ for(let i = 0; i < gridSize; i++){
     for(let x = 0; x < gridSize; x++){
         const x = document.createElement('div');
         x.classList.add('gridSquare');
-
+        //x.style.backgroundColor = backgroundColor;
+        x.style.minHeight = `${squareSize}px`;
+        x.style.minWidth = `${squareSize}px`;
+        
         i.appendChild(x);
 
     }

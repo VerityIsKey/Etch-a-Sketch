5dfb6525ce999511e743306a471feef8981151ca
Jared <jaredhessworks@gmail.com>
2023-06-26 18:32:49 -0400

    the prompt now sets the grid size

--- index.js
+++ index.js
@@ -1,26 +1,43 @@
 const gridContainer = document.querySelector('.gridContainer');
-
+const btn = document.querySelector('.btn');
 let gridSize = 16;
+createGrid(gridSize);
 
-let squareSize = 960 / gridSize;
+function createGrid(gridSize){
+    let squareSize = 960 / gridSize;
+    for(let i = 0; i < gridSize; i++){
+        const i = document.createElement('div');
+        i.classList.add('parentSquares');
 
-
-for(let i = 0; i < gridSize; i++){
-    const i = document.createElement('div');
-    i.classList.add('parentSquares');
-
- 
-
-    gridContainer.appendChild(i);
+        gridContainer.appendChild(i);
     
-    for(let x = 0; x < gridSize; x++){
-        const x = document.createElement('div');
-        x.classList.add('gridSquare');
-        //x.style.backgroundColor = backgroundColor;
-        x.style.minHeight = `${squareSize}px`;
-        x.style.minWidth = `${squareSize}px`;
+        for(let x = 0; x < gridSize; x++){
+            const x = document.createElement('div');
+            x.classList.add('gridSquare');
+            //x.style.backgroundColor = backgroundColor;
+            x.style.minHeight = `${squareSize}px`;
+            x.style.minWidth = `${squareSize}px`;
         
-        i.appendChild(x);
+            i.appendChild(x);
 
+        }
     }
 }
+
+function removeCurrentGrid(){
+    const parentSquares = document.querySelectorAll('.parentSquares');
+    
+    parentSquares.forEach(function(parentSquare){
+        parentSquare.remove();
+    });
+}
+
+function promptGrid(gridSize){
+    gridSize = parseInt(prompt("Enter the grid size"));
+    removeCurrentGrid();
+    createGrid(gridSize);
+}
+
+btn.addEventListener('click', promptGrid);
+
+
